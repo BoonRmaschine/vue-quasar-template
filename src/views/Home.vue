02d0da77cf3347d8680d3806f1da2c25c8e353d1
Stephan Koglin-Fischer <step.fischer@gmail.com>
2020-01-18 15:37:10 +0100

    Imported the github package successfully

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@boonrmaschine/vue-quasar-package-template';
 
 export default {
   name: 'home',
